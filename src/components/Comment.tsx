a8666002cc3d3430d2b6417988c660b3f1208f23
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2024-11-12 10:37:51 -0300

    aprimorando-aplicacao

---
+++ src/components/Comment.tsx
@@ -0,0 +1,59 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export const Comment = ({ content, onDeleteComment }: CommentProps) => {
+  const [likeCount, setLikeCount] = useState(0);
+
+  const handleDeleteComment = () => {
+    onDeleteComment(content);
+  };
+
+  const handleLikeComment = () => {
+    setLikeCount((state) => state + 1);
+  };
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/fracanabia.png"
+        alt=""
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>
+                Wesley Fracanábia <span>(você)</span>
+              </strong>
+              <time
+                title="11 de Novembro de 2024 ás 08:00"
+                dateTime="2024-11-11 08:00:00"
+              >
+                Cerca de 2h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp size={20} />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+};
